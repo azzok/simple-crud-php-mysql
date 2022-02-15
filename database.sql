b2e1b2a6e4952b97a6a54ce13ebf6a089ca9ff2c
Azzok <azzok17@gmail.com>
2022-02-15 15:22:46 +0530

    :books: Initial commit

---
+++ database.sql
@@ -0,0 +1,11 @@
+create database simple_crud_php_mysql;
+
+use simple_crud_php_mysql;
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL auto_increment,
+  `name` varchar(100) NOT NULL,
+  `age` int(3) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  PRIMARY KEY  (`id`)
+);
